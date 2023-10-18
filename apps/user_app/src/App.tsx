78b28ea66c62f3c2abc2b27df33aa7e4c7db85b9
sarath <sarathkp1998@gmail.com>
2023-10-18 23:51:28 +0530

    adding the react app fro user

---
+++ apps/user_app/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>here comes the user page , from login to cart</div>
+  )
+}
+
+export default App
